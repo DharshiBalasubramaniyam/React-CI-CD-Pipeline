4d34961b2d51dc511d44189ac5d1293ad654f12e
Dharshi Balasubramaniyam <139672976+DharshiBalasubramaniyam@users.noreply.github.com>
2024-07-03 09:04:40 +0530

    Update ApiConfig.jsx

--- src/api-service/ApiConfig.jsx
+++ src/api-service/ApiConfig.jsx
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://load-balancer-for-ci-ci-practice-1398898249.us-east-1.elb.amazonaws.com";
+// Below is the DNS Name of the load balancer configured for the backend springboot application.
 
-// export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "http://load-balancer-for-ci-ci-practice-1398898249.us-east-1.elb.amazonaws.com";
